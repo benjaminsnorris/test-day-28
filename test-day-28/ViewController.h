526b9b4746ff9a570c7f70e2f6b7ac9060daad7c
Ben Norris <ben@bsn.io>
2014-10-25 09:09:41 -0600

    Initial Commit

---
+++ test-day-28/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test-day-28
+//
+//  Created by Ben Norris on 10/25/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test-day-28/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test-day-28
+//
+//  Created by Ben Norris on 10/25/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
